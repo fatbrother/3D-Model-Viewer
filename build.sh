dbc5a5286ec3a8962beb3b0991fdf5f0216e6526
fatbrother <kevin92422@gmail.com>
2023-10-03 22:24:45 +0800

    fix: remove not use command

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ root_path=$(cd `dirname $0`; pwd)
 mkdir -p $root_path/build
 mkdir -p $root_path/build/bin
 cd $root_path/build
-cmake cmake -B . -S . -DCMAKE_TOOLCHAIN_FILE=$root_path/vcpkg/scripts/buildsystems/vcpkg.cmake ../
+cmake -B . -S . -DCMAKE_TOOLCHAIN_FILE=$root_path/vcpkg/scripts/buildsystems/vcpkg.cmake ../
 cmake --build .
